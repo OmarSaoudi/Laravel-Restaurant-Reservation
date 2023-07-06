d24a0692e7a769268c82184174623b17d0a5e724
Omar Saoudi <saoudiomar518@gmail.com>
2023-07-06 19:55:01 +0100

    restaurant reservation

---
+++ app/Http/Controllers/Admin/DoctorController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Doctor;
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $doctors = Doctor::all();
+        return view('admin.doctors.index', compact('doctors'));
+    }
+
+}
---
+++ app/Http/Controllers/Frontend/DoctorController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Doctor;
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    public function index()
+    {
+        $doctors = Doctor::all();
+        return view('doctors.index', compact('doctors'));
+    }
+}
